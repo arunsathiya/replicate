81350f6970bee6b309b77c2785e89213bce7238b
Arun <arun@arun.blog>
2025-01-18 03:17:06 +0530

    ci: fix release workflow

--- replicate.php
+++ replicate.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Plugin Name: Replicate
+ * Plugin URI: https://github.com/arunsathiya/replicate
+ * Description: Integration with Replicate API for WordPress
+ * Version: 0.1.0
+ * Author: arun
+ * Author URI: https://github.com/arunsathiya
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
